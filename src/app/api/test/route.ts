855f4b48abc3a0a69d3e16ea4d1d976c4d41af1c
KubaPC <ivo.urbanski@gmail.com>
2025-06-03 20:26:11 +0200

    removed test

--- src/app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import dbConnect from '@/app/lib/mongodb';
-
-export async function GET() {
-    await dbConnect();
-    return NextResponse.json({ ok: true });
-}
