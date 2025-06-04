64bc2e20200255656547aee124b323c80eae24da
KubaPC <ivo.urbanski@gmail.com>
2025-06-04 09:55:22 +0200

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,11 @@ export function middleware(request: NextRequest) {
 
     const isPublicPath = path === '/' || path === '/terms'
 
-    const token = request.cookies.get('next-auth.session-token')?.value || ''
+    // Check for both cookie names
+    const token =
+        request.cookies.get('next-auth.session-token')?.value ||
+        request.cookies.get('__Secure-next-auth.session-token')?.value ||
+        '';
 
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
